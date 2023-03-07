3c0b055088b736bbb23f5c9655d1cdb1340ac9c6
djost <djost@spotify.com>
2023-03-07 10:52:27 +0100

    lesson 25 done

--- packages/annotations/variables.ts
+++ packages/annotations/variables.ts
@@ -37,6 +37,23 @@ const json = '{"x":10, "y":20}';
 const coordinates: {x:number; y:number} = JSON.parse(json);
 console.log(coordinates);
 
+// 2) when we declare a variable on one line and initialize it later
+let words = ['red','green','blue'];
+let foundWord: boolean;
 
+for (let i = 0; i < words.length; i++) {
+  if (words[i] === 'green') {
+    foundWord = true;
+  } 
+}
 
+// 3) When we want a variable to have a type that can't be inferred
+let numbers = [-10, -1, 12];
+// watch out, bad code for the sake of explanation
+let numberAboveZero: boolean | number = false;
 
+for (let i = 0; i < numbers.length; i++) {
+  if (numbers[i] > 0) {
+    numberAboveZero = numbers[i];
+  }
+}
