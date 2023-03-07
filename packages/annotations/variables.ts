7c768a104a82b22990ab60e500496081fef39d5f
djost <djost@spotify.com>
2023-03-07 10:30:25 +0100

    lesson 20 done

--- packages/annotations/variables.ts
+++ packages/annotations/variables.ts
@@ -24,6 +24,11 @@ let point: { x: number; y: number } = {
 };
 
 // function
+// JS syntax: const fn_name = (parameter) => {function body with implicit return}
+// TS syntax: const fn_name: (fn_arguments: arguments_type) => fn_return_type = (parameter: parameter_type) => { fn_body }
+// the part "(i: number) => void" is the fn annotation for arguments and return types
 const logNumber: (i: number) => void = (i: number) => {
   console.log("i :", i);
 };
+
+
