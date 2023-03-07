06e622b31880684463ad079f43fba69b82514792
djost <djost@spotify.com>
2023-03-07 13:19:31 +0100

    removed vscode config

--- packages/annotations/variables.ts
+++ packages/annotations/variables.ts
@@ -14,7 +14,7 @@ let myNumbers: number[] = [1, 2, 3];
 let truths: boolean[] = [true, true, false];
 
 // classes
-class Car {}
+class Car { }
 let car: Car = new Car();
 
 // object literal
@@ -26,7 +26,7 @@ let point: { x: number; y: number } = {
 // function
 // JS syntax: const fn_name = (parameter) => {function body with implicit return}
 // TS syntax: const fn_name: (fn_arguments: arguments_type) => fn_return_type = (parameter: parameter_type) => { fn_body }
-// the part "(i: number) => void" is the fn annotation for arguments and return types
+// the part "(i: number) => void" is the variable annotation for a function with arguments and return types
 const logNumber: (i: number) => void = (i: number) => {
   console.log("i :", i);
 };
@@ -34,17 +34,17 @@ const logNumber: (i: number) => void = (i: number) => {
 // when to use annotations
 // 1) When using a function that returns the 'any' type, like JSON.parse()
 const json = '{"x":10, "y":20}';
-const coordinates: {x:number; y:number} = JSON.parse(json);
+const coordinates: { x: number; y: number } = JSON.parse(json);
 console.log(coordinates);
 
 // 2) when we declare a variable on one line and initialize it later
-let words = ['red','green','blue'];
+let words = ['red', 'green', 'blue'];
 let foundWord: boolean;
 
 for (let i = 0; i < words.length; i++) {
   if (words[i] === 'green') {
     foundWord = true;
-  } 
+  }
 }
 
 // 3) When we want a variable to have a type that can't be inferred
