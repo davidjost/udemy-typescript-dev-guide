4aec0e98add5dc47827a00f503aa3af220fa904a
djost <djost@spotify.com>
2023-03-10 16:00:49 +0100

    lesson 31 added

--- packages/annotations/functions.ts
+++ packages/annotations/functions.ts
@@ -18,8 +18,35 @@ const logger = (message: string): void => {
   console.log("message: ", message);
 };
 
+// lesson 29
 const throwError = (message: string): void => {
   if (!message) {
     throw new Error(message);
   }
 };
+
+// lesson 30
+const todaysWeather = {
+  date: new Date(),
+  weather: "sunny",
+};
+
+// ES2016 syntax with destructuring
+const logWeather = ({
+  date,
+  weather,
+}: {
+  date: Date;
+  weather: string;
+}): void => {
+  console.log("date: ", date);
+  console.log("weather: ", weather);
+};
+
+// ES2015 syntax
+const logWeather2 = ({ date, weather }) => {
+  console.log("date: ", date);
+  console.log("weather: ", weather);
+};
+
+logWeather(todaysWeather);
